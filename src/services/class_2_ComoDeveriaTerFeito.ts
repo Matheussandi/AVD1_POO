249088b4746b662171f31bbf06e2cb639f8aaad1
Matheus <zTeussandi@hotmail.com>
2021-09-23 21:45:11 -0300

    Add files via upload

---
+++ src/services/class_2_ComoDeveriaTerFeito.ts
@@ -0,0 +1,34 @@
+class ConvetTempo {
+    static minutoParaSegundo(n: number) {
+        return n * 60 + ' segundos'
+    }
+
+    static horaParaMinuto(n: number) {
+        return n * 60 + ' minutos'
+    }
+
+    static diaParaHoras(n: number) {
+        return n * 24 + ' horas'
+    }
+
+    static semanaParaDias(n: number) {
+        return n * 7 + ' dias'
+    }
+
+    static mesParaDias(n: number) {
+        return n * 30 + ' dias'
+    }
+
+    static anoParaDias(n: number) {
+        return n * 365.25 + ' dias'
+    }
+}
+
+
+ 
+console.log(ConvetTempo.minutoParaSegundo(1))
+console.log(ConvetTempo.horaParaMinuto(1))
+console.log(ConvetTempo.diaParaHoras(1))
+console.log(ConvetTempo.semanaParaDias(1))
+console.log(ConvetTempo.mesParaDias(1))
+console.log(ConvetTempo.anoParaDias(1))
